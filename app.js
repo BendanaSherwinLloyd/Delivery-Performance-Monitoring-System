4b61c48453cd1bc533b84e605a78afb174d9fbd8
RianWilliamAlday <23-19976@g.batstate-u.edu.ph>
2026-05-22 15:11:45 +0800

    First Commit

---
+++ app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import 'dotenv/config.js';
+import authRoutes from "./routes/authRoute.js";                                                                                
+
+const app = express();
+
+//*miiddleware
+app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+})
+
+try {
+    app.listen(process.env.PORT || 3000, () => {     
+        console.log(`Listening to port ${process.env.PORT || 3000}...`);
+    });
+} catch (e) {
+    console.log(e);
+}
+
+export default performanceRoutes;
+
